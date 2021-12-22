e41492d0722ef6058ee5f59cb29575dc85250894
Amrhub <amr_ahmed65@hotmail.com>
2021-12-22 10:05:24 +0200

    initial commits

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # healthcare-ML-API
+
+url: <br>
+endpoint: "/data": <br>
+
+## <b>_POST_<b> request:
+
+```js
+{
+"name": string,
+"device_id": string
+}
+```
+
+## <b>_GET_<b> request:
+
+receives:
+array of objects `for example`:
+
+```json
+[
+  {
+    "name": "Nasseratic",
+    "device_id": "1"
+  },
+  {
+    "name": "Nasseratic",
+    "device_id": "2"
+  }
+]
+```
---
+++ main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+
+from pydantic import BaseModel 
+
+from pymongo import MongoClient
+
+app = FastAPI()
+
+client = MongoClient("mongodb+srv://healthcaregp:healthcaregp@cluster0.qq548.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = client.healthcaredb
+
+deviceDataCollection = db["deviceData"]
+
+class DeviceData(BaseModel):
+    name: str
+    device_id: str
+
+@app.get("/")
+def read_root():
+    return {"Hello": "HealthCare"}
+
+
+@app.post('/data')
+def post_data( data: DeviceData):
+    deviceDataCollection.insert_one(data.dict())
+    return data
+
+@app.get('/data')
+def get_data():
+    list = []
+    for doc in deviceDataCollection.find({}, {"_id": 0}):
+        list.append(doc)
+    return list
+
