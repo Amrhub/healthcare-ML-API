437a26a27c29acfa24824e33e3370c7fa23d388b
Amrhub <amr_ahmed65@hotmail.com>
2022-01-27 20:15:39 +0200

    update: data format

--- main.py
+++ main.py
@@ -1,32 +1,38 @@
 from fastapi import FastAPI
 
-from pydantic import BaseModel 
+from pydantic import BaseModel
 
 from pymongo import MongoClient
 
 app = FastAPI()
 
-client = MongoClient("mongodb+srv://healthcaregp:healthcaregp@cluster0.qq548.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+client = MongoClient(
+    "mongodb+srv://healthcaregp:healthcaregp@cluster0.qq548.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 db = client.healthcaredb
 
 deviceDataCollection = db["deviceData"]
 
+
 class DeviceData(BaseModel):
-    name: str
-    device_id: str
+    id: str
+    heartRate: float
+    spo2: float
+    temp: float
+    ESG: list
+    GPS: list
+
 
 @app.get("/")
 def read_root():
-    return {"Hello": "HealthCare"}
+    return {"Hello": "HealthCare 2.0"}
 
 
 @app.post('/data')
-def post_data( data: DeviceData):
+def post_data(data: DeviceData):
     deviceDataCollection.insert_one(data.dict())
     return data
+
 
 @app.get('/data')
 def get_data():
     return list(deviceDataCollection.find({}, {"_id": 0}))
-
-
